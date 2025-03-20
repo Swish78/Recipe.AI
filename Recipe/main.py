10f7a71d3917793ba35c88c031d51465706ef277
Swayam Patil <98048555+Swish78@users.noreply.github.com>
2025-03-20 19:52:11 +0530

    Add main

---
+++ Recipe/main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_cors import CORS
+from routes.invoice_routes import invoice_bp
+from routes.ingredient_routes import ingredient_bp
+from routes.recipe_routes import recipe_bp
+from database import JSONEncoder
+
+app = Flask(__name__)
+CORS(app)
+
+app.json_encoder = JSONEncoder
+
+# Register blueprints
+app.register_blueprint(invoice_bp, url_prefix='/api')
+app.register_blueprint(ingredient_bp, url_prefix='/api')
+app.register_blueprint(recipe_bp, url_prefix='/api')
+
+app.get("/")(lambda: "Welcome to the Recipe API!")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
