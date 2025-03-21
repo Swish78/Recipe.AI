ad82457fd5f256b1fa5b41d309d77d1bb6147e13
Swayam Patil <98048555+Swish78@users.noreply.github.com>
2025-03-22 01:00:13 +0530

    Add validate connection

--- Recipe/database.py
+++ Recipe/database.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 import json
 from bson.objectid import ObjectId
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from config import MONGODB_URI, DB_NAME, INGREDIENTS_COLLECTION, RECIPES_COLLECTION
 
 
@@ -9,15 +10,27 @@ class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, ObjectId):
             return str(obj)
-        if isinstance(obj, datetime):
-            return obj.isoformat()
-        if isinstance(obj, date):
+        if isinstance(obj, datetime, date):
             return obj.isoformat()
         return json.JSONEncoder.default(self, obj)
 
 
-# Database connection
-client = MongoClient(MONGODB_URI, TLS=True, tlsAllowInvalidCertificates=True)
-db = client[DB_NAME]
-ingredients_collection = db[INGREDIENTS_COLLECTION]
-recipes_collection = db[RECIPES_COLLECTION]
+client = None
+db = None
+ingredients_collection = None
+recipes_collection = None
+
+def init_db():
+    global client, db, ingredients_collection, recipes_collection
+    try:
+        client = MongoClient(MONGODB_URI, TLS=True, tlsAllowInvalidCertificates=True, serverSelectionTimeoutMS=5000, server_api=ServerApi('1'))
+        client.server_info()
+        db = client[DB_NAME]
+        ingredients_collection = db[INGREDIENTS_COLLECTION]
+        recipes_collection = db[RECIPES_COLLECTION]
+        print("Successfully connected to MongoDB.")
+    except Exception as e:
+        print(f"Failed to connect to MongoDB: {str(e)}")
+        raise
+
+init_db()
